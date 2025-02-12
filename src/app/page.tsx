92f4a288d12240a345bc0189a9414f0152c27de2
Gustavo <101915322+Gustavo2202@users.noreply.github.com>
2025-02-12 19:29:32 -0300

    add link btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import './globals.scss';
 import './Components/style/Home.scss';
 import { Experiece } from "./Components/Experiece/Experiece";
 import Info from "./Components/Information/Info";
+
 export default function Home() {
+  
   return (
     <main className="container">
       <Header />
@@ -12,7 +14,9 @@ export default function Home() {
 
       <div className="buttons">
         <div className="social"></div>
-        <button className="btn btnPrimary" >Contate me</button>
+        <button className="btn btnPrimary" > 
+       <a href="https://api.whatsapp.com/qr/QUCAWRSNWU4QO1?autoload=1&app_absent=0" target="_blank">Contate me</a>
+          </button>
       </div>
 
     </main>
